dfc468c5fccde0f422384479fc2533712a4af2de
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-22 14:16:48 +0600

    deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import VideoPlayer from '../components/VideoPlayer'; 
 
 export default function Home() {
   return (
@@ -7,7 +7,6 @@ export default function Home() {
       <div className="Home-container">
         <div className="home-text">
           <h3>Welcome to the world of home goods</h3>
-
           <p>
             Create your dream home: At HomeHarmony you will find
             everything you need to create a cozy and stylish environment
@@ -15,9 +14,8 @@ export default function Home() {
             our range will please even the most discerning customers.
           </p>
         </div>
-        
+        <VideoPlayer />
       </div>
-      
     </div>
   );
 }
