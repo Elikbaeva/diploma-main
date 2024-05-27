95f99b32531baf9fc5033ae6a2cb69df3c8010dd
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-27 14:44:28 +0600

    added a

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
               rel="noreferrer"
               href="elikbaevasedep77gamil.com"
             >
-              elikbaevasedep77gamil.com
+              elikbaevasedep77@gmail.com
             </a>
           </span>
         </div>
@@ -67,7 +67,10 @@ const Contact = () => {
             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
           </svg>
           <span className="contact-text">
-            Address: 1 Esenina, Karakol, Kyrgyzstan
+          Address:{" "}
+            <a href="#">
+             1 Esenina, Karakol, Kyrgyzstan
+            </a>
           </span>
         </div>
       </div>
