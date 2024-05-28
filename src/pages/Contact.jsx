8772c8bff41352ff84a1726537f54d71b4f67229
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-28 10:57:32 +0600

    added new conponent ContactForm

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -74,6 +74,7 @@ const Contact = () => {
           </span>
         </div>
       </div>
+      
       <div className="Contacts_map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d46793.32304975421!2d74.61601280000001!3d42.860544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1714988710851!5m2!1sru!2skg" title="Контактная информация" width="100%" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
