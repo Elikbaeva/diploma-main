9ba268a9b6c690a5aa5138699b7ed18825c6c3c6
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-29 13:58:32 +0600

    added category

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -11,7 +11,7 @@ export default function CategoryList() {
   const output = categories.map((category) => {
     if (category.name === 'all') {
       return (
-        
+
         <li className="all" key={category.id}>
           <NavLink to={`/categories/${category.slug}`}>
             {category.name}
