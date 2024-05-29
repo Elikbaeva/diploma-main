c520732c10d381f855416369b0891b75ce3e3db7
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-29 14:28:03 +0600

    deleted backdrop

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -5,9 +5,7 @@ import "./Drawer.css";
 
 function Drawer({ open, toggle }) {
   const handleItemClick = () => {
-    toggle(); // Toggle the drawer state when an item is clicked
-
-    
+    toggle(); 
   };
 
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
@@ -16,7 +14,7 @@ function Drawer({ open, toggle }) {
     <div className={drawerClassNames}>
       <div onClick={toggle} className="backdrop"></div>
       <div className="content">
-        <button onClick={toggle} className="close-btn">X</button> {/* Close button */}
+        <button onClick={toggle} className="close-btn">X</button> {}
         <Logo />
         <Nav onItemClick={handleItemClick} />
       </div>
