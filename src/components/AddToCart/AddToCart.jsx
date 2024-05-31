eb0bac3ee6c565bc1b0b65b4a722bab380ea0d50
Elikbaeva <elikbaevasedep77@gmail.com>
2024-05-31 10:01:26 +0600

    changed fill

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -22,7 +22,7 @@ export default function AddToCart({ product }) {
           xmlns="http://www.w3.org/2000/svg"
           width="25"
           height="25"
-          fill="currentColor"
+          fill="black"
           className="bi bi-cart-plus-fill"
           viewBox="0 0 16 16"
         >
